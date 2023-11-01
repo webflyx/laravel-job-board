32e216009831ea40717c82453c95dbedf75d4925
webflyx <zavala180395@gmail.com>
2023-11-01 15:03:49 +0200

    Set EmployerFactory, DBSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Job;
+use App\Models\User;
+use App\Models\Employer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,8 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(100)->create();
+
+        $users = User::all()->shuffle();
+
+        for( $i = 0; $i < 20; $i++ ) {
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id,
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for( $i = 0; $i < 100; $i++ ) {
+            Job::factory()->create(
+                [
+                    'employer_id' => $employers->random()->id,
+                ]
+            );
+        }
+
         // \App\Models\User::factory(10)->create();
-         \App\Models\Job::factory(100)->create();
+        // \App\Models\Job::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
